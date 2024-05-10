b7cc8b93742ddaf586241a7d48b984f647a37ffd
Anastasia <ndthwm@yandex.ru>
2024-05-10 20:09:13 +0700

    Добавлена авторизация

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs');
+const user = require('../models/user');
+
+const login = (req, res) => {
+  const {email, password} = req.body;
+
+  user
+  .findUserByCredentials(email, password)
+  .then(user => {
+    res.status(200).send({ _id: user._id, username: user.username, email: user.email });
+  })
+  .catch(error => {
+    res.status(401).send({ message: error.message });
+  })
+}
+
+module.exports = {login};
